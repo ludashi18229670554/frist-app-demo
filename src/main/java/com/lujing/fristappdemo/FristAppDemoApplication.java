94ce4e6905835cc39f5a8484a3e37756d6ecad4a
ludashi18229670554 <ludashi18229670554@users.noreply.github.com>
2018-02-08 15:54:04 +0800

    Initial commit

---
+++ src/main/java/com/lujing/fristappdemo/FristAppDemoApplication.java
@@ -0,0 +1,12 @@
+package com.lujing.fristappdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FristAppDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FristAppDemoApplication.class, args);
+	}
+}
